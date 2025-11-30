211199bd4a0ee4dd1e9f3adbd947190e13c9818d
arashhappyteams <arash@happyteams.ai>
2025-11-30 13:24:09 -0800

    Change base path in Vite config to root

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/hannanejad-website/' // <-- put your repo name here
+  base: '/' // <-- put your repo name here
 });
